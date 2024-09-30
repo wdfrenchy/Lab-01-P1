bf0303ecccc5427fff4256b8a58115cf265252c2
Dawson French <wdfrench2006@gmail.com>
2024-09-30 15:53:34 +0100

    Create lab-01-p1.main.kts

---
+++ lab-01-p1.main.kts
@@ -0,0 +1,15 @@
+// Returns the perfect square of a given number
+fun perfectSquare(num: Int): Int {
+    return num * num
+
+    // If adjusted for inputs where the input type could be a variable type other than an Int
+    /* when (num) {
+        is Int -> return num * num
+        else -> return println("Invalid input. Please try again.")
+    } */
+}
+
+// Runs the program
+fun main() {
+    println(perfectSquare(16))
+}
