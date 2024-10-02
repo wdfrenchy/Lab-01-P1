e94d4bcdcd8a1103fb02dd076a8783d4dec7bac4
Dawson French <wdfrench2006@gmail.com>
2024-10-02 15:28:52 +0100

    Update lab-02-p4.main.kts

--- lab-02-p4.main.kts
+++ lab-02-p4.main.kts
@@ -6,13 +6,13 @@ import khoury.EnabledTest
 import khoury.runEnabledTests
 import khoury.reactConsole
 
-/* Creates an enumeration with parameters */
+// Creates an enumeration with parameters
 enum class SongState {ABC, EASY_AS, BABY}
 
-/* Creating the state variable */
+// Creating the state variable
 var state: SongState = SongState.ABC
 
-/* Transfers the enumeration's parameters to text */
+// Transfers the enumeration's parameters to text
 fun stateToText(state: SongState): String {
     when (state) {
         SongState.ABC -> return "A, B, C"
@@ -20,7 +20,7 @@ fun stateToText(state: SongState): String {
         SongState.BABY -> return "A, B, C, 1, 2, 3, baby, you and me, girl!"
     }
 }
-/* Plays the next state according to the conditions already satisfied */
+// Plays the next state according to the conditions already satisfied
 fun nextSongState(state: SongState, input: String): SongState {
     if(input.equals("exit")) {
         return SongState.BABY
@@ -30,17 +30,17 @@ fun nextSongState(state: SongState, input: String): SongState {
     }
 }
 
-/* Returns whether or not the current state is the final state */
+// Returns whether or not the current state is the final state
 fun isDone(state: SongState): Boolean {
     return state == SongState.BABY
 }
 
-/* Loops through the second state's text */
+// Loops through the second state's text
 fun loop() {
     reactConsole(state, ::stateToText, ::nextSongState, ::isDone)
 }
 
-/* Runs the program */
+// Runs the program
 fun main() {
     loop()
 }
